998ae18eb3f15eb2b5ed4d9ce3cddeea006f8c47
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2024-04-04 19:35:57 -0300

    add borg init log

--- create-repos.sh
+++ create-repos.sh
@@ -3,6 +3,7 @@ source constants.sh
 
 for folder_to_backup in "${folders_to_backup[@]}"; do
   repo_folder="$BACKUP_FOLDER"/"$folder_to_backup"
+  echo borg init repo in "$repo_folder"
   mkdir --parent "$repo_folder"
   borg init --encryption repokey "$repo_folder"
 done
