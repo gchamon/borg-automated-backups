65788f84a4a3316969df5bb928889b05fc23707a
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2024-04-04 19:55:02 -0300

    add env feature to customize deployments

--- constants.sh
+++ constants.sh
@@ -1,4 +1,10 @@
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 folders_to_backup=(home etc srv boot opt usr)
+ENV_FILE="$SCRIPT_DIR"/env
+if [ -f "$ENV_FILE" ]; then
+    source "$ENV_FILE"
+fi
 
 COLOR_RED='\e[31m'
 COLOR_NONE='\e[0m'
