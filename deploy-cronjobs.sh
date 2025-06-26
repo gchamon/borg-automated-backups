f5e6c0124539faf4231784bea8907e744aa1f4dc
Gabriel Chamon <gchamon@tuta.io>
2025-06-26 12:31:06 -0300

    single cronjob that executes run_all.sh instead of a job per folder

--- deploy-cronjobs.sh
+++ deploy-cronjobs.sh
@@ -7,8 +7,8 @@ mkdir --parents "$borg_backup_scripts_folder"
 mkdir --parents $borg_backup_logs_folder
 
 for folder_to_backup in "${FOLDERS_TO_BACKUP[@]}"; do
-  backup_script="$borg_backup_scripts_folder/$folder_to_backup.sh"
-  cat > "$backup_script" <<CRONJOB
+    backup_script="$borg_backup_scripts_folder/$folder_to_backup.sh"
+    cat >"$backup_script" <<CRONJOB
 #!/usr/bin/env bash
 exec > >(tee -a $borg_backup_logs_folder/$folder_to_backup.log) 2>&1
 echo "[start] \$(date)"
@@ -38,14 +38,15 @@ echo "[end] \$(date)"
 echo "[runtime] \$((end-start))"
 
 CRONJOB
-
-  chmod +x "$backup_script"
-  echo "$CRON_SCHEDULE root /bin/bash $backup_script" > "/etc/cron.d/borg_backup_$folder_to_backup"
+    chmod +x "$backup_script"
 done
 
-cat > "$borg_backup_scripts_folder"/run_all.sh <<SCRIPT
+cat >"$borg_backup_scripts_folder"/run_all.sh <<SCRIPT
 for folder_to_backup in ${FOLDERS_TO_BACKUP[@]}; do
   backup_script="$borg_backup_scripts_folder/\$folder_to_backup.sh"
   bash \$backup_script
 done
 SCRIPT
+chmod +x "$borg_backup_scripts_folder"/run_all.sh
+
+echo "$CRON_SCHEDULE root /bin/bash $borg_backup_scripts_folder/run_all.sh" >"/etc/cron.d/borg_backup"
